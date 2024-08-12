5fbc1bcdb3401d566b6b2aaf24e63d6593cabb81
Al Gorithm <web.resource.305@gmail.com>
2024-08-12 12:09:25 +0100

    Used conventions from example

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
 // Define a simple route
-app.get('/', (req, res) => {
+app.get('/app', (req, res) => {
   res.send('Hello, World!');
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
