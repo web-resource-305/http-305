8a18c21c6465a7e50a87c93e2cf0ae498d49445c
web-resource-305 <web.resource.305@gmail.com>
2026-04-06 00:18:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ logger.info(`Logging level: ${logger.level}`);
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.set("trust proxy", true);
+app.set("trust proxy", 1); // true = all proxies
 app.use("/pxy/dl", rateLimit({
   windowMs: 60_000,
   max: 60,
